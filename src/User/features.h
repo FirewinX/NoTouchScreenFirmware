0432cd486b7c4e4a1274d05379b874cf8324fea8
FirewinX <75076307+FirewinX@users.noreply.github.com>
2021-03-15 13:58:33 +0100

    Update features.h

--- src/User/features.h
+++ src/User/features.h
@@ -24,7 +24,7 @@
 #endif
 
 // Enable SPI data received indicator
-#define SPI_DATA_RECEIVED_INDICATOR
+//#define SPI_DATA_RECEIVED_INDICATOR
 
 // Rotary knob long press duration for SPI restart
 #define SPI_RESTART_KNOB_PRESS_DURATION 3
